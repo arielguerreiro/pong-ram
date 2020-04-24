0198a8c774b3eb40c174e1c243c00fb2b7a7ac3d
PSXBRosa <61952980+PSXBRosa@users.noreply.github.com>
2020-04-24 00:20:28 -0300

    final final

--- load.py
+++ load.py
@@ -1,6 +1,7 @@
 import pickle
 import numpy as np
 from objects import Environment
+from main import discretize
 
 def load_table(file):
 	with open(file, 'rb') as pickle_in:
@@ -10,14 +11,16 @@ def load_table(file):
 env = Environment()
 Q = load_table('model.pickle')
 
-NUMBER_OF_EPISODES = 5
+NUMBER_OF_EPISODES = 1
 
 for i in range(NUMBER_OF_EPISODES):
 	done = False
 	s = env.reset()
+	s = discretize(s)
 	while not done:
 		action = np.argmax(Q[s])
 		s2, reward, done, _ = env.step(action)
+		s2 = discretize(s2)
 		env.render()
 		s = s2
 
